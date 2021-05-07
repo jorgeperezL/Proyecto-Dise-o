ad142c5c612b0773b24a99069e4e133dd32094b6
mikeherrero21 <58515139+mikeherrero21@users.noreply.github.com>
2021-05-07 09:50:33 +0200

    Decorator -- falta cambiar las frases para que tengan sentido
    Strategy-- hacer que el random funcione bien

--- Proyecto DesignPatterns/src/PatronStrategy/Contexto.java
+++ Proyecto DesignPatterns/src/PatronStrategy/Contexto.java
@@ -1,5 +1,18 @@
 package PatronStrategy;
 
 public class Contexto {
-
+	private Strategies algoritmo;
+	
+	public Contexto() {
+		
+	}
+	
+	public void execute() {
+		algoritmo.executeType();
+	}
+	
+	public void setType(Strategies algoritmo) {
+		this.algoritmo=algoritmo;
+	}
+	
 }
