e29390df1af34cabd39ec1006d8a663be5b15eee
mikeherrero21 <58515139+mikeherrero21@users.noreply.github.com>
2021-05-20 21:53:52 +0200

    m

---
+++ Proyecto DesignPatterns/src/PatronState/Normal.java
@@ -0,0 +1,11 @@
+package PatronState;
+
+public class Normal implements EstadoJugador{
+
+	@Override
+	public void mostrarEstado() {
+		System.out.println("estoy bien ramona, de tus alumnos favoritos!!!");
+		
+	}
+
+}
